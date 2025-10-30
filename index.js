a99f4371a68de3dbaa70d1cbe148dab2106b779f
Deniz Can Tokpunar <denizzcn12@gmail.com>
2025-10-30 14:01:44 +0300

    a

---
+++ index.js
@@ -0,0 +1,25 @@
+console.log("selam")
+
+const adres="İzmir";
+
+
+function topla(x,y){
+    return x+y
+}
+
+const add=(x,y)=>x+y;
+
+let x=10
+if(x==10){
+        console.log("X ONMUŞ")
+}
+
+
+var adi="a";
+var adi="b";
+
+let yas="20";
+// let yas="30"; olmaz
+ 
+
+
