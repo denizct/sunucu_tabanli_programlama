37ad0efca565e5143e809aa4624c9b7f60ba1804
Deniz Can Tokpunar <denizzcn12@gmail.com>
2025-11-02 20:19:35 +0300

    part1

---
+++ ddd.js
@@ -0,0 +1,26 @@
+console.log("selammm");
+
+const degisken1="ali";
+console.log(degisken1);
+// const degisken1="ali2"; olmaz
+
+let degisken2="ela";
+// let degisken2="ela2"; olmaz
+degisken2="ela3";    
+console.log(degisken2)
+
+var degisken3="onur"
+var degisken3="onur2"
+console.log(degisken3)
+
+function topla(x, y) {
+  return x + y;
+}
+
+function carp(x,y) {
+    return x*y;
+}
+console.log(carp(5,5));
+
+const carp2 = (x,y) => x*y;
+console.log(carp2(6,6))
