c595cbd90a64034c60c4a304278bdb074aac4d54
Deniz Can Tokpunar <denizzcn12@gmail.com>
2025-11-02 20:48:14 +0300

    tekrar

--- ddd.js
+++ ddd.js
@@ -24,3 +24,21 @@ console.log(carp(5,5));
 
 const carp2 = (x,y) => x*y;
 console.log(carp2(6,6))
+
+
+let x = 1;
+if (x == 1) {
+    console.log("x babadır");
+}
+else{
+  console.log("x abidir");
+}
+
+
+let toplam = 0;
+for (let i = 1; i < 4; i++) {
+    
+    toplam += i;
+    console.log(toplam)
+}
+
